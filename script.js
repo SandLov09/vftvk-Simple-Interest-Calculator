dc325c18b035521a423cd0524cd9d74f226a6877
SandLov09 <info@cebron-itda.ch>
2022-03-01 10:35:00 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    //assign variables
+
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+
+    //calc and convert
+
+    let interest = principal * years * rate / 100;
+    let year = new Date().getFullYear()+parseInt(years);
+
+    //catch false input
+
+    if(principal <= 0){
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    return false;
+    }
+    //show result 
+
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>, <br> at an interest rate of <mark>${rate}</mark> %<br> You will receive an amount of <mark>${interest}</mark> in the year <mark>${year}</mark> <br>`
+
 }
-        
+
+//function that updates the slider value and displays it to the screen
+function updateRate(){
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}        
+
+
+
+    //Slider update funtion
+
+    function updateRate() {
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
